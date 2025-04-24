362d6ddb5afeec7efdca527b797f787121c2fc8e
Wen Cheng <wencheng31502@gmail.com>
2025-04-25 03:55:59 +1000

    Added path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/PortfolioSite',
+  assetPrefix: '/PortfolioSite/',
   images: {
     unoptimized: true,
   },

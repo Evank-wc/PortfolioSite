cb2e4f1ad33fe3a42b8f68e30991d3de7228402f
Wen Cheng <wencheng31502@gmail.com>
2025-04-25 03:40:23 +1000

    Fixed deployment issues

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/PortfolioSite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/PortfolioSite/' : '',
 }
 
 module.exports = nextConfig 
